6e7b1d2bb59f497b6f2e8006add2d391bdfd9dbd
aarshpatel1 <aarsh18patel@gmail.com>
2024-10-24 11:19:53 +0530

    question_9.py bug fixed

--- question_9.py
+++ question_9.py
@@ -1,18 +1,27 @@
-x = int(input("Enter the number: "))
-if x > 0:
-    x = str(x)
-    n = ""
-    for i in range(len(x) - 1, -1, -1):
-        n += x[i]
+# question - 9 (check palindrome number)
 
-    x = int(x)
-    n = int(n)
-    print(x)
-    print(n)
-    if x == n:
-        print(True)
+number = int(input("Enter the number: "))
+
+
+def is_palindrome(x):
+    if x == 0:
+        return True
+    elif x > 0:
+        x = str(x)
+        n = ""
+        for i in range(len(x) - 1, -1, -1):
+            n += x[i]
+
+        x = int(x)
+        n = int(n)
+        print(x)
+        print(n)
+        if x == n:
+            return True
+        else:
+            return False
     else:
-        print(False)
-else:
-    print(False)
+        return False
 
+
+print(is_palindrome(number))
