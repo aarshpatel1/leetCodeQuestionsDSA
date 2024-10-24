fe876009f6010067ddfb04c5905362eb2da683d0
aarshpatel1 <aarsh18patel@gmail.com>
2024-10-24 15:34:16 +0530

    question_13.py add. roman number to integer numbers. still in progress. almost there.

--- question_13.py
+++ question_13.py
@@ -2,16 +2,15 @@
 
 roman = input("Enter the Roman Number: ").upper()
 values = {
-    "I": "1",
-    "V": "5",
-    "X": "10",
-    "L": "50",
-    "C": "100",
-    "D": "500",
-    "M": "1000"
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
 }
 
-integer_number = ""
 valid_number = True
 for i in roman:
     if i not in values:
@@ -19,44 +18,52 @@ for i in roman:
         break
 
 roman += "N"
+integer_number = 0
+print(roman)
 
+i = 0
 if valid_number:
-    for i in range(len(roman)):
-        if roman[i] == "M":
-            integer_number = str(int(integer_number) + int(values["M"]))
-        elif roman[i] == "D":
-            integer_number = str(int(integer_number) + int(values["D"]))
+    # for i in range(len(roman)):
+    while i <= (len(roman) - 1):
+        if roman[i] == "D":
+            integer_number += values["D"]
+            i += 1
         elif roman[i] == "C" and roman[i + 1] == "M":
-            integer_number = str(int(integer_number) - int(values["C"]) + int(values["M"]))
+            integer_number += (values["M"] - values["C"])
+            i += 2
+        elif roman[i] == "M":
+            integer_number += values["M"]
+            i += 1
         elif roman[i] == "C" and roman[i + 1] == "D":
-            integer_number = str(int(integer_number) - int(values["C"]) + int(values["D"]))
+            integer_number += (values["D"] - values["C"])
+            i += 2
         elif roman[i] == "C":
-            integer_number = str(int(integer_number) + int(values["C"]))
+            integer_number += values["C"]
+            i += 1
         elif roman[i] == "L":
-            integer_number = str(int(integer_number) + int(values["L"]))
+            integer_number += values["L"]
         elif roman[i] == "X" and roman[i + 1] == "C":
-            integer_number = str(int(integer_number) - int(values["X"]) + int(values["C"]))
+            integer_number += (values["C"] - values["X"])
+            i += 2
         elif roman[i] == "X" and roman[i + 1] == "L":
-            integer_number = str(int(integer_number) - int(values["X"]) + int(values["L"]))
+            integer_number += (values["L"] - values["X"])
+            i += 2
         elif roman[i] == "X":
-            integer_number = str(int(integer_number) + int(values["X"]))
+            integer_number += values["X"]
+            i += 1
+        elif roman[i] == "V":
+            integer_number += values["V"]
+            i += 1
         elif roman[i] == "I" and roman[i + 1] == "X":
-            integer_number = str(int(integer_number) - int(values["I"]) + int(values["X"]))
+            integer_number += (values["X"] - values["I"])
+            i += 2
         elif roman[i] == "I" and roman[i + 1] == "V":
-            integer_number = str(int(integer_number) - int(values["I"]) + int(values["V"]))
+            integer_number += (values["V"] - values["I"])
+            i += 2
         elif roman[i] == "I":
-            integer_number = str(int(integer_number) + int(values["X"]))
-
-        #
-        # elif roman[i] == "I" and roman[i + 1] == "V":
-        #     integer_number += str(int(values["V"]) - int(values["I"]))
-        # elif roman[i] == "I" and roman[i + 1] == "X":
-        #     integer_number += str(int(values["X"]) - int(values["I"]))
-        # elif roman[i] == "I" and roman[i + 1] == "I" and roman[i + 2] == "I":
-        #     integer_number += str(int(values["I"]) * 3)
-        # elif roman[i] == "I" and roman[i + 1] == "I":
-        #     integer_number += str(int(values["I"]) * 2)
-        # elif roman[i] == "I":
-        #     integer_number += values["I"]
+            integer_number += values["I"]
+            i += 1
+        else:
+            i+=1
 
 print(integer_number)
