ee351cc99572c132e3e05fdc31a73af1468a314c
aarshpatel1 <aarsh18patel@gmail.com>
2024-10-24 15:54:51 +0530

    question_13.py complete. add 2 methods of solution

--- question_13.py
+++ question_13.py
@@ -1,6 +1,9 @@
 # question - 13 (Roman to Integer)
 
 roman = input("Enter the Roman Number: ").upper()
+
+
+# method - 1
 values = {
     "I": 1,
     "V": 5,
@@ -68,3 +71,50 @@ if valid_number:
             i += 1
 
 print(integer_number)
+
+# method - 2
+# i = 0
+# while i <= (len(roman) - 1):
+#     if roman[i] == "D":
+#         integer_number += 500
+#         i += 1
+#     elif roman[i] == "C" and roman[i + 1] == "M":
+#         integer_number += (1000 - 100)
+#         i += 2
+#     elif roman[i] == "M":
+#         integer_number += 1000
+#         i += 1
+#     elif roman[i] == "C" and roman[i + 1] == "D":
+#         integer_number += (500 - 100)
+#         i += 2
+#     elif roman[i] == "C":
+#         integer_number += 100
+#         i += 1
+#     elif roman[i] == "L":
+#         integer_number += 50
+#         i += 1
+#     elif roman[i] == "X" and roman[i + 1] == "C":
+#         integer_number += (100 - 10)
+#         i += 2
+#     elif roman[i] == "X" and roman[i + 1] == "L":
+#         integer_number += (50 - 10)
+#         i += 2
+#     elif roman[i] == "X":
+#         integer_number += 10
+#         i += 1
+#     elif roman[i] == "V":
+#         integer_number += 5
+#         i += 1
+#     elif roman[i] == "I" and roman[i + 1] == "X":
+#         integer_number += (10 - 1)
+#         i += 2
+#     elif roman[i] == "I" and roman[i + 1] == "V":
+#         integer_number += (5 - 1)
+#         i += 2
+#     elif roman[i] == "I":
+#         integer_number += 1
+#         i += 1
+#     else:
+#         i += 1
+#
+# print(integer_number)
