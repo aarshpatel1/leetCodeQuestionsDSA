8e6e837245c373391cc96a8ec323aae48690f528
aarshpatel1 <aarsh18patel@gmail.com>
2024-10-25 19:09:52 +0530

    question_20.py some bugs are still there

--- question_20.py
+++ question_20.py
@@ -1,15 +1,28 @@
+# question - 20 (Valid Parentheses)
+
 parentheses = input("Enter the parentheses only: ")
+
+
+def check_closing(opening, index):
+    if opening == "(":
+        closing = ")"
+    elif opening == "{":
+        closing = "}"
+    elif opening == "[":
+        closing = "]"
+    else:
+        return True
+
+    for j in range(index, len(parentheses)):
+        if parentheses[j] == closing:
+            return True
+    return False
 
 
 def check_parentheses(s):
     for i in range(len(s) - 1):
-        if s[i] == "(" and s[i + 1] != ")":
+        if not check_closing(s[i], i):
             return False
-        elif s[i] == "{" and s[i + 1] != "}":
-            return False
-        elif s[i] == "[" and s[i + 1] != "]":
-            return False
-
     return True
 
 
