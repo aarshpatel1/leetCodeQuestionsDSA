8323a4b7a5f40852e3534b8cd507fd7e4be0e5d2
aarshpatel1 <aarsh18patel@gmail.com>
2024-10-25 19:43:46 +0530

    question_20.py some bugs are still there

--- question_20.py
+++ question_20.py
@@ -1,16 +1,39 @@
 parentheses = input("Enter the parentheses only: ")
 
 
-def check_parentheses(s):
-    for i in range(len(s) - 1):
-        if s[i] == "(" and s[i + 1] != ")":
-            return False
-        elif s[i] == "{" and s[i + 1] != "}":
-            return False
-        elif s[i] == "[" and s[i + 1] != "]":
-            return False
+def check_closing(opening, index):
+    if opening == "(":
+        closing = ")"
+    elif opening == "{":
+        closing = "}"
+    elif opening == "[":
+        closing = "]"
+    else:
+        return True
+    for j in range(index, len(parentheses)):
+        if parentheses[j] == closing:
+            return True
+    return False
 
-    return True
+
+def check_parentheses(s):
+    openings = ["(", "{", "["]
+    closings = [")", "}", "]"]
+    if len(s) == 1 or s[len(s) - 1] in openings:
+        return False
+    else:
+        for i in range(len(s) - 1):
+            if s[i] == "(" and s[i + 1] != ")" and s[i + 1] in closings:
+                return False
+            elif s[i] == "{" and s[i + 1] != "}" and s[i + 1] in closings:
+                return False
+            elif s[i] == "[" and s[i + 1] != "]" and s[i + 1] in closings:
+                return False
+            else:
+                if not check_closing(s[i], i):
+                    return False
+
+        return True
 
 
 print(check_parentheses(parentheses))
